8255bbbe0f199a3a4166e31a3e3411276dc49eb7
Agarwal Mahaveer <mveer.jaipur@gmail.com>
2022-04-06 06:12:59 +0530

    Container integration with other shared

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,4 +1,1 @@
-import faker from "faker";
-
-const cartText = `<div> you have ${faker.random.number()} item in your cart`;
-document.querySelector("#dev-cart").innerHTML = cartText;
+import("./bootstrap");
